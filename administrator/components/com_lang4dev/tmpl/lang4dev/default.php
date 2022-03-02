f2e972586085d895b4911642df411ceb0144a877
ThomasFinnern <ThomasFinnernE5570@users.noreply.github.com>
2022-03-02 22:31:57 +0100

    Added test button

--- administrator/components/com_lang4dev/tmpl/lang4dev/default.php
+++ administrator/components/com_lang4dev/tmpl/lang4dev/default.php
@@ -17,6 +17,35 @@ use Joomla\CMS\Language\Associations;
 use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Session\Session;
 
+$button = new stdClass();
+$button->link = Route::_('index.php?option=com_lang4dev2&task=test.dummySearch');
+$button->classButton = 'dummyTask';
+$button->textTitle = 'test search';
+$button->textInfo = 'first search for own lnguage com_...';
+
+$imageClass='image';
+
+function DisplayButton($button)
+{
+	// <button type="button" class="btn btn-primary">Primary</button>
+	?>
+	<div class="rsg2-icon-button-container">;
+		<button type="button" class="btn btn-warning">
+		<a href="<?php echo $button->link; ?>" class="<?php echo $button->classButton; ?>">
+			<figure class="rsg2-icon">
+				<span class="<?php echo $imageClass; ?> icoMoon" style="font-size:30px;"></span>
+				<figcaption class="rsg2-text">
+	                <div class="maint-title"><strong><?php echo $button->textTitle; ?></strong></div>
+	                <div class="maint-text"><?php echo $button->textInfo; ?></div>
+	            </figcaption>
+		    </figure>
+		</a>
+		</button>
+	</div>
+	<?php
+}
+
+
 ?>
 
     <form action="<?php echo Route::_('index.php?option=com_lang4dev'); ?>"
@@ -35,8 +64,14 @@ use Joomla\CMS\Session\Session;
 
 		?>
 
-<h2>Lang4Dev</h2>
+		<h2>Lang4Dev</h2>
 
+
+
+	    <?php DisplayButton($button); ?>
+
+	    <input type="hidden" name="task" value="" />
+	    <?php echo HTMLHelper::_('form.token'); ?>
 
     </form>
 
