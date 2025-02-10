8377671e7d1e2b4c30226311842a8e5e8b5aaa9d
thomas.finnern <thomas.finnern@walter-machines.de>
2025-02-10 12:53:54 +0100

    Small improvements

--- plugins/webservices/lang4dev/lang4dev.php
+++ plugins/webservices/lang4dev/lang4dev.php
@@ -2,14 +2,19 @@
 
 
 use Joomla\CMS\Plugin\CMSPlugin;
+//use Joomla\CMS\Router\ApiRouter;
 use Joomla\CMS\Router\ApiRouter;
 
 class PlgWebservicesLang4Dev extends CMSPlugin
 {
     public function onBeforeApiRoute(&$router)
     {
-        $router->createCRUDRoutes('v1/lang4dev', 'lang4dev', ['component' => 'com_lang4dev']);
+        $router->createCRUDRoutes(
+			'v1/lang4dev', 
+			'lang4dev', 
+			['component' => 'com_lang4dev']);
     }
+	
 }
 
 
